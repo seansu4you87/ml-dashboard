5296f25b995483cd0fafde8fd99fc0994dccf4d9
Sean Yu <seansu4you87@gmail.com>
2012-12-28 11:22:19 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileLearnDashboard::Application.config.secret_token = '4faf6e6525b4ad8e1edb23e7984579f66abcc02d9fc6934a6c4868ac344706fea09d671193dc30294e8493e454b540d95825ec856535fdf6e23c68dd8e644cdd'
