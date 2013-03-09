b1f7dd90ee7836f99d0b4dacd01bd4cd36cd75cc
Sean Yu <seansu4you87@gmail.com>
2013-03-09 15:54:55 -0800

    testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
 
   def hours
     # @purchases = Purchase.search(params)
-    @hours = Hour.search(params)
+    @hours = [Hour.search(params).first]
   end
 
 end
